f9bf8276275daf4b375f3847e4be87409dc0693e
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2021-12-03 12:17:10 +0100

    shuffle function

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function spinToWin(n) {
     if (n <= -1 || n >= 10000000) {
-      var num = "Error";
-      document.querySelector("div.output").innerHTML = num;
-      return num;
+      document.querySelector("div.output").innerHTML = "Error";
     } else {
         if(document.getElementsByClassName("form-check-input").inlineCheckbox1.checked == true) {
             var num = Math.floor(Math.random() * (n - 0 + 1));
@@ -18,9 +16,36 @@ function spinToWin(n) {
 
 var numbersArray = [];
 
-function pushNumber(n) {
-    numbersArray.push(n);
+function push(n, min, max) {
+    if (document.querySelector('input#number').value != '' && document.querySelector('input#min-number').value == '' && document.querySelector('input#max-number').value == '') {
+        numbersArray.push(n);
+        document.querySelector("input#number").value = '';
+        document.querySelector("div.previewInputCurrent").innerHTML = numbersArray[numbersArray.length - 1];
+        document.querySelector("div.previewInputFull").innerHTML = numbersArray;
+    } else if (document.querySelector('input#min-number').value != '' && document.querySelector('input#max-number').value != '' && document.querySelector('input#number').value == '') {
+        for (let i = min; i <= max; i++) {
+            numbersArray.push(i);
+            document.querySelector("div.previewInputFull").innerHTML = numbersArray;
+        }
+    } else {
+        document.querySelector("div.output").innerHTML = "Error";
+    }
 }
+
+// The Old functions
+
+// function pushRange(min, max) {
+//     for (let i = min; i < max; i++) {
+//         numbersArray.push(i);
+//     }
+// }
+
+// function pushNumber(n) {
+//     numbersArray.push(n);
+//     document.querySelector("input#number").value = '';
+//     document.querySelector("div.previewInputCurrent").innerHTML = numbersArray[numbersArray.length - 1];
+//     document.querySelector("div.previewInputFull").innerHTML = numbersArray;
+// }
 
 function arrRandomizer(array) {
     var newArr = [];
@@ -31,4 +56,6 @@ function arrRandomizer(array) {
         newArr.push(splicedNum);
     }
     document.querySelector("div.output").innerHTML = newArr;
+    document.querySelector("div.previewInputCurrent").innerHTML = 'Placeholder';
+    document.querySelector("div.previewInputFull").innerHTML = 'Placeholder';
 }
