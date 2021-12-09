8eb83ce872d9e721423f6378f2749131e428334b
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2021-12-09 13:44:50 +0100

    added functionality

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
-let output = document.querySelector("div.output");
-let preview = document.querySelector("div.previewInputCurrent");
+let output = document.querySelector('div.output');
+let baseNum = document.querySelector('input#number');
+let minNum = document.querySelector('input#min-number');
+let maxNum = document.querySelector('input#max-number');
+let checkboxInput = document.getElementsByClassName('form-check-input');
+let confirmBtn = document.querySelector("button.confirm");
+let numbersArray = [];
 
 function spinToWin(n) {
-    let checkboxInput = document.getElementsByClassName("form-check-input");
-
-    if (n <= -1 || n >= 1e7) {
-      output.innerHTML = "Error";
-    } else {
-        if(checkboxInput.inlineCheckbox1.checked == true) {
-            let num = Math.floor(Math.random() * (n - 0 + 1));
-            output.innerHTML = num;
-        } else if (checkboxInput.inlineCheckbox1.checked == false) {
-            let num = Math.floor(Math.random() * (n - 1 + 1)) + 1;
-            output.innerHTML = num;
-        }
+  if (n <= -1 || n >= 1e7) {
+    output.innerHTML = 'Error';
+  } else {
+    if (checkboxInput.inlineCheckbox1.checked == true) {
+      let num = Math.floor(Math.random() * (n - 0 + 1));
+      output.innerHTML = num;
+    } else if (checkboxInput.inlineCheckbox1.checked == false) {
+      let num = Math.floor(Math.random() * (n - 1 + 1)) + 1;
+      output.innerHTML = num;
     }
   }
-
-var numbersArray = [];
+}
 
 function push(n, min, max) {
-    let baseNum = document.querySelector('input#number');
-    let minNum = document.querySelector('input#min-number');
-    let maxNum = document.querySelector('input#max-number');
-
-    if (baseNum.value != '' && minNum.value == '' && maxNum.value == '') {
-        numbersArray.push(n);
-        baseNum.value = '';
-        output.innerHTML = numbersArray;
-        preview.innerHTML = numbersArray[numbersArray.length - 1];
-    } else if (minNum.value != '' && maxNum.value != '' && baseNum.value == '') {
-        for (let i = min; i <= max; i++) {
-            numbersArray.push(i);
-            output.innerHTML = numbersArray;
-        }
-    } else {
-        output.innerHTML = "Error";
+  if (baseNum.value != '' && minNum.value == '' && maxNum.value == '') {
+    numbersArray.push(n);
+    output.innerHTML = numbersArray;
+  } else if (minNum.value != '' && maxNum.value != '' && baseNum.value == '') {
+    for (let i = min; i <= max; i++) {
+      numbersArray.push(i);
     }
+    output.innerHTML = numbersArray;
+    confirmBtn.disabled = true;
+
+  } else {
+    output.innerHTML = 'Error';
+  }
 }
 
 function arrRandomizer(array) {
-    var newArr = [];
-    while(array.length) {
-        var random = Math.floor(Math.random() * array.length);
-        var splicedArr = array.splice(random, 1);
-        var splicedNum = splicedArr[0];
-        newArr.push(splicedNum);
-    }
-    numbersArray = newArr;
-    output.innerHTML = numbersArray;
-    preview.innerHTML = 'Placeholder';
+  var newArr = [];
+  while (array.length) {
+    var random = Math.floor(Math.random() * array.length);
+    var splicedArr = array.splice(random, 1);
+    var splicedNum = splicedArr[0];
+    newArr.push(splicedNum);
+  }
+  numbersArray = newArr;
+  output.innerHTML = numbersArray;
 }
 
-// Old functions
+function resetArray() {
+    numbersArray = [];
+    output.innerHTML = '';
+    confirmBtn.disabled = false;
+}
 
-// function pushRange(min, max) {
-//     for (let i = min; i < max; i++) {
-//         numbersArray.push(i);
-//     }
-// }
+function resetText() {
+    baseNum.value = '';
+    maxNum.value = '';
+    minNum.value = '';
+}
 
-// function pushNumber(n) {
-//     numbersArray.push(n);
-//     document.querySelector("input#number").value = '';
-//     document.querySelector("div.previewInputCurrent").innerHTML = numbersArray[numbersArray.length - 1];
-//     document.querySelector("div.previewInputFull").innerHTML = numbersArray;
-// }
+setInterval(function(){
+    if(baseNum.value != '') {confirmBtn.disabled = false;}
+        }, 100)
